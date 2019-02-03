264040206ec1e30d0adde346748eb94c12efb80c
Micah Monfrini <ffrock905@gmail.com>
2019-02-03 14:05:10 -0600

    make put request

--- app.js
+++ app.js
@@ -19,7 +19,17 @@ const data = {
 
 // Add new post
 
-http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     console.log(post)
+//   }
+// });
+
+// Update post
+
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
   if(err) {
     console.log(err)
   } else {
