e748792ed43a4717a1f0ab69d8cf5a8589f12276
Micah Monfrini <ffrock905@gmail.com>
2019-02-03 14:01:16 -0600

    add data

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const http = new easyHTTP;
 
 // Get posts
 
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-  if(err) {
-    console.log(err)
-  } else {
-    console.log(posts)
-  }
-});
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if(err) {
+//     console.log(err)
+//   } else {
+//     console.log(posts)
+//   }
+// });
+
+// Data for post request
+
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+}
 
 // Add new post
 
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, posts) {
   if(err) {
     console.log(err)
   } else {
