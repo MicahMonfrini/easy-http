f7104f7d80b1fe406e8946d895b80906b20e970c
Micah Monfrini <ffrock905@gmail.com>
2019-02-03 14:06:27 -0600

    create delete request logic

--- easyhttp.js
+++ easyhttp.js
@@ -48,3 +48,17 @@ easyHTTP.prototype.put = function(url, data, callback) {
 
 
 // DELETE request
+easyHTTP.prototype.delete = function(url, callback) {
+  this.http.open('DELETE', url, true);
+
+  let self = this
+  this.http.onload = function() {
+    if(self.http.status === 200) {
+      callback(null, self.http.responseText);
+    } else {
+      callback('Error:' + self.http.status);
+    }
+  }
+
+  this.http.send();
+}
